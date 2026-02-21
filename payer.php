358eb7b882e1621e49b073d37ecf1aea78aa0463
SATOSHI669 <albanouedraogo44@gmail.com>
2026-02-21 14:43:17 +0000

    Update payer.php

--- payer.php
+++ payer.php
@@ -1,7 +1,6 @@
 <?php
 // Ton jeton PawaPay (sandbox)
-$PAWAPAY_TOKEN = "eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJ0dCI6IkFBVCIsInN1YiI6IjE2OTA2IiwibWF2IjoiMSIsImV4cCI6MjA4NzIwMDk0OCwiaWF0IjoxNzcxNjY4MTQ4LCJwbSI6IkRBRixQQUYiLCJqdGkiOiIyZmIyOWE5OS03ZTQ0LTRjNzUtOGRjMC1hYTY5NzNhNjhlMzUifQ.FTSuf5JiXPTRUiGA5fHHZLv7DTzkhX-DdxFj3lxpbswKQD6-n3_nPjhvbzV1cTPPwYBQ-xf6zFRK9xk7YygxGA"
-; // Mets ton vrai jeton
+$PAWAPAY_TOKEN = "ton_jeton_pawapay_ici"; // Mets ton vrai jeton
 
 // Fonction pour générer un UUID v4 valide
 function gen_uuid() {
@@ -32,7 +31,7 @@ $data = [
         'amount' => $amount,
         'currency' => $currency
     ],
-    'countries' => 'BFA', // Pour fixer le pays (Sénégal)
+    'country' => 'SEN', // Pour fixer le pays (Sénégal)
     'reason' => 'Paiement commande'
 ];
 
