f406a0976145b2d013c988742d7b5157276fa096
SATOSHI669 <albanouedraogo44@gmail.com>
2026-02-21 10:54:33 +0000

    Update payer.php

--- payer.php
+++ payer.php
@@ -31,7 +31,7 @@ $data = [
         'amount' => $amount,
         'currency' => $currency
     ],
-    'country' => 'SEN', // Pour fixer le pays (Sénégal)
+    
     'reason' => 'Paiement commande'
 ];
 
@@ -57,6 +57,7 @@ echo json_encode([
     'requete_envoyee' => $data
 ]);
 ?>
+
 
 
 
